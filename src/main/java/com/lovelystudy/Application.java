7ed5e50d201b067dcc27d3cbbe60d8165f306231
arisosoftwaredeveloper <arisosoftware@gmail.com>
2020-02-22 11:11:10 -0500

    jdk8 vs jdk11 ; elastic process # error ; mysql setup
    1) jdk 11 can't run jdk8 in sprint boot
    2) IllegalStateException: availableProcessors is already set to [4]

--- src/main/java/com/lovelystudy/Application.java
+++ src/main/java/com/lovelystudy/Application.java
@@ -16,6 +16,8 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 public class Application {
 
 	public static void main(String[] args) {
+		System.setProperty("es.set.netty.runtime.available.processors", "false");
+		
 		SpringApplication.run(Application.class, args);
 	}
 
